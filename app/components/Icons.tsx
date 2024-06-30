d14bdafff8263b6361019c982f9b2c306b22e508
SERGIO <40707060+LEWISGREGG@users.noreply.github.com>
2024-06-30 16:38:06 -0400

    Add files via upload

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -19,9 +19,9 @@ return (
     alt="PE"
     width={220}
     height={220}
-    className="animate-spin-slow "
+    className="animate-spin-slow"
     />
-    <Link href="mailto:serhio.kushnirenko@gmail.com" target={"_blank"} 
+    <Link href="mailto:serhii.kushnirenko@engineeringassociates.com" target={"_blank"} 
 className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-blue-950 text-gray-50 
 shadow-md border border-solid border-blue-950 w-[88px] h-[88px] rounded-full font-semibold hover:bg-gray-200 hover:text-blue-950 
 transition duration-300 ease-out hover:ease-in m md:text-center dark:hover:shadow-amber-600">
